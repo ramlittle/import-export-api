2f1030512d5a41ff792fb410f432c84053ec2bed
ramelitomartinez <ramelitomartinez@gmail.com>
2023-06-01 14:35:41 -0400

    feat: tested initial APIs and worked

--- routes/users.js
+++ routes/users.js
@@ -5,25 +5,32 @@ const router = express.Router();
 // Model
 const User = require('../models/Users');
 
-
-router.get('/:id', ( request, response ) => {
-    User.findOne(
-        { _id: request.params.id },
-        {
-            password: 0
-        })
-    .populate({ path: 'posts' })
-    .exec( (err, result) => {
-        console.log( result );
-        if( typeof result === 'object' ){
-            response.send( result );
-        }
-    });
+//return all users
+router.get('/', async (request, response)=>{
+    try{
+   const results = await User.find({})
+   .exec();
+   response.send(results);
+   }catch(error){
+       response.status(500).send({status:'server error'})
+   }
 });
 
+//return one user
+router.get('/:id', async (request, response)=>{
+    try{
+    const userId=request.params.id;
+   const results = await User.findOne({_id:userId})
+   .exec();
+   response.send(results);
+   }catch(error){
+       response.status(500).send({status:'server error'})
+   }
+});
 
-// Update a post
+// Update a user
 router.put('/:id', ( request, response ) => {
+    try{
     const userId = request.params.id;
     User.updateOne(
         { _id: userId },
@@ -33,19 +40,27 @@ router.put('/:id', ( request, response ) => {
             response.send({ status: "User has been updated" });
         }
     });
+}catch(error){
+    response.status(500).send({status:'server error'})
+}
 });
 
 
-// Delete a post
+// Delete a user
 router.delete('/:id', ( request, response ) => {
-    User.deleteOne({ _id: request.params.id })
-    .then( result => {
-        if( result.deletedCount === 1 ){
-            response.send({
-                status: "User has been deleted"
-            });
-        }
-    });
+    try{
+
+        User.deleteOne({ _id: request.params.id })
+        .then( result => {
+            if( result.deletedCount === 1 ){
+                response.send({
+                    status: "User has been deleted"
+                });
+            }
+        });
+    }catch(error){
+        response.status(500).send({status:'server error'})
+    }
 });
 
 
