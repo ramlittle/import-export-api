bf6a86ce4aa50dfdf1899f963e7c95fb4f97cc7b
Ramelito Martinez <ramelitomartinez@gmail.com>
2023-06-07 10:55:44 +0800

    made api of update one password work

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
+// Dependencies
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require('bcrypt');
 
 // Model
 const User = require('../models/Users');
@@ -31,25 +32,44 @@ router.get('/:id', async (request, response)=>{
 // Update a user
 router.put('/:id', ( request, response ) => {
     try{
-    const userId = request.params.id;
-    User.updateOne(
-        { _id: userId },
-        { $set: { ...request.body } })
-    .then( result => {
-        if( result.modifiedCount === 1 ){
-            response.send({ status: "User has been updated" });
-        }
-    });
-}catch(error){
-    response.status(500).send({status:'server error'})
-}
+        const userId = request.params.id;
+        User.updateOne(
+            { _id: userId },
+            { $set: { ...request.body } })
+        .then( result => {
+            if( result.modifiedCount === 1 ){
+                response.send({ status: "User has been updated" });
+            }
+        });
+    }catch(error){
+        
+    }
 });
+
+//Update a user password
+router.put('/password/:id', async (request, response) => {
+    try{
+        const hashedPassword = await bcrypt.hash( request.body.password, 10 );
+        const userId = request.params.id;
+        let result = await User.updateOne(
+            { _id: userId },
+            { $set: { ...request.body,
+                    password: hashedPassword
+            }}
+        );
+        if (result.modifiedCount === 1) {
+        response.status(200).send({ status: "Password Update is successful!" });
+        } 
+    }catch(error){
+        response.status(500).send({status:'server error'})
+    }
+});
+
 
 
 // Delete a user
 router.delete('/:id', ( request, response ) => {
     try{
-
         User.deleteOne({ _id: request.params.id })
         .then( result => {
             if( result.deletedCount === 1 ){
