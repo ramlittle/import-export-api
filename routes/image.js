6cd4d9dad08b68c41b6fee47d9248f33dac74968
ramlittle <ramelitomartinez@gmail.com>
2023-07-04 17:31:45 +0800

    feat: fix uploading of images

--- routes/image.js
+++ routes/image.js
@@ -1,24 +1,24 @@
-const express=require('express');
+const express = require('express');
 const router = express.Router();
-const multer =require('multer');
+const multer = require('multer');
 const path = require('path')
 const Image = require('../models/Image');
 
-const storage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,'../Images')//this is the folder that would contain your images
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './Images')//this is the folder that would contain your images
     },
-    filename: (req,file,cb)=>{
-        console.log('here is the file',file)
-        cb(null, Date.now()+ path.extname(file.originalname))//when the file is accepted, it will show as [date]-fileoriginalname
+    filename: (req, file, cb) => {
+        console.log('here is the file', file)
+        cb(null, Date.now() + path.extname(file.originalname))//when the file is accepted, it will show as [date]-fileoriginalname
     }
 })
-const upload=multer({storage: storage})
+const upload = multer({ storage: storage })
 
 //the actual api to upload image, the one after upoad.single is the name of the input where you will grab the file
 //ex. input name ='name'
-router.post('/upload',upload.single('image'),(req,res)=>{
-    console.log('here is the file you gave',req.image)
+router.post('/upload', upload.single('file'),  (req, res) => {
+    console.log('here is the file you gave', req.file)
     res.send('image uploaded');
 })
 
