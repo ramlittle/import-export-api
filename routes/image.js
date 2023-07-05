3b89ffa9174f7d7af62134b6a9535a74fcf5b603
ramlittle <ramelitomartinez@gmail.com>
2023-07-05 08:42:44 +0800

    feat: examined and added if others fields can be captured

--- routes/image.js
+++ routes/image.js
@@ -4,21 +4,25 @@ const multer = require('multer');
 const path = require('path')
 const Image = require('../models/Image');
 
+let imageFileName='';
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, './Images')//this is the folder that would contain your images
     },
     filename: (req, file, cb) => {
         console.log('here is the file', file)
-        cb(null, Date.now() + path.extname(file.originalname))//when the file is accepted, it will show as [date]-fileoriginalname
+        imageFileName=Date.now() + path.extname(file.originalname);
+        cb(null, imageFileName)//when the file is accepted, it will show as [date]-fileoriginalname
+
     }
 })
 const upload = multer({ storage: storage })
 
 //the actual api to upload image, the one after upoad.single is the name of the input where you will grab the file
 //ex. input name ='name'
-router.post('/upload', upload.single('file'),  (req, res) => {
-    console.log('here is the file you gave', req.file)
+router.post('/upload', upload.single('file'), (req, res) => {
+    console.log('here is the file you gave',req.body)
+    console.log('here is the image file name',imageFileName)
     res.send('image uploaded');
 })
 
